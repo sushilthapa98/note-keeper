318ce2cd7d2f31c6816c03c4b8f1d0cbf9680da1
Sushil Thapa <thapasushil6@gmail.com>
2026-02-03 15:29:29 +0545

    fix: add session check to route, data access layer in query route

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,16 +1,27 @@
 import postgres from "postgres";
+import { verifySession } from "../lib/dal";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 async function listNotes() {
+  const session = await verifySession();
+  if (!session) {
+    return null;
+  }
+
   const data = await sql`
-    SELECT * FROM notes;
+    SELECT * FROM notes WHERE user_id = ${session.userId};
   `;
 
   return data;
 }
 
 export async function GET() {
+  const session = await verifySession();
+  if (!session) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   try {
     return Response.json(await listNotes());
   } catch (error) {
