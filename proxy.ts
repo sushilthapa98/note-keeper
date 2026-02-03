e013803373fe5a5e8f3508d0e843cc99d5a36e97
Sushil Thapa <thapasushil6@gmail.com>
2026-02-03 18:14:44 +0545

    add new logic to proxy ts based on next auth

---
+++ proxy.ts
@@ -0,0 +1,26 @@
+import { auth } from "@/auth";
+import { NextAuthRequest } from "next-auth";
+import { NextResponse } from "next/server";
+
+// we can export a named function proxy or a default function
+// below, auth is a function from next-auth, we are just passing a callback function that can be used to write any custom logic
+// now, this req is not just next request, it is a request object from next-auth
+export default auth((req: NextAuthRequest) => {
+  const isLoggedIn = !!req?.auth?.user;
+  const isOnLoginPage = req.nextUrl.pathname.startsWith("/login");
+  const isProtectedRoute = req.nextUrl.pathname === "/";
+
+  if (isLoggedIn && isOnLoginPage) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (isProtectedRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
